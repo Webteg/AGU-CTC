af7290df3259c09007e58286bd41330dcc3a9301
LAB-pesquisa <roger_amaros@hotmail.com>
2018-12-17 15:26:37 -0200

    1.3

--- lattes.py
+++ lattes.py
@@ -10,11 +10,22 @@ from time import sleep
 class extractLattes:
     def __init__(self, url=None):
         self.ids = []
-        self.conn = sqlite3.connect("/home/lab-pesquisa/Área de Trabalho/AGU-CTC/ids.db")
+        self.conn = sqlite3.connect("/home/lab-pesquisa/Desktop/AGU-CTC/ids.db")
         self.cursor = self.conn.cursor()
         self.atual_url = "http://buscatextual.cnpq.br/buscatextual/busca.do?metodo=forwardPaginaResultados&registros=0;10&query=%28+%2Bidx_grd_area_atua%3A%22ENGENHARIAS%22++++++++%2Bidx_atuacao_prof_anterior%3Abra+%2Bidx_atuacao_prof_anterior%3Asu+%2Bidx_atuacao_prof_anterior%3Asc+%2Bidx_nme_inst_ativ_prof%3Auniversidade+federal+de+santa+catarina++++++%2Bidx_particao%3A1+%2Bidx_nacionalidade%3Ae%29+or+%28+%2Bidx_grd_area_atua%3A%22ENGENHARIAS%22++++++++%2Bidx_atuacao_prof_anterior%3Abra+%2Bidx_atuacao_prof_anterior%3Asu+%2Bidx_atuacao_prof_anterior%3Asc+%2Bidx_nme_inst_ativ_prof%3Auniversidade+federal+de+santa+catarina++++++%2Bidx_particao%3A1+%2Bidx_nacionalidade%3Ab%29&analise=cv&tipoOrdenacao=null&paginaOrigem=index.do&mostrarScore=false&mostrarBandeira=true&modoIndAdhoc=null"
         self.headers_agent = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+    def filter(self, id):
+        data =  self.recovery_by_id(id)
+        if not data:
+            return 'No data'
+        else:
+            if "Universidade Federal de Santa Catarina" in data[0]:
+                return True
+            else:
+                return False
+
 
     def bsobj(self):
         try:
@@ -29,16 +40,18 @@ class extractLattes:
 
     def recovery_by_id(self, id):
         sleep(4)
+        max_data = []
         url = "http://buscatextual.cnpq.br/buscatextual/visualizacv.do?id=" + str(id)
         try:
             req = requests.get(url, headers=self.headers_agent)
             bsObj = BeautifulSoup(req.content, 'html.parser')
             description =  bsObj.findAll('p', class_='resumo')[0].text
             endereco = bsObj.find_all('div',{'class':'layout-cell layout-cell-9'})[2].text
-            local = bsObj.find_all('div',{'class':'inst_back'})[0].text
-            act = bsObj.find_all('div',{'class':'layout-cell-pad-5'})
-            print(act)
-            #return [description,endereco]  
+            for children in bsObj.find_all('div',{'class':'title-wrapper'})[6].children:
+                print(children.get_text)
+                
+                
+
         except Exception as e:
             return e
 
@@ -72,15 +85,15 @@ class extractLattes:
             return self.recovery_id_recursive(lastPage)
         except Exception as e:
             return e
-    def filter_by_location(self):
-        ids = [[x[0],self.recovery_by_id(x[0])[1]] for x in self.get_data()]
-        ids_on_ufsc = [ x for x in ids if "ufsc" or "engenheria" or "ctc" in x[1]]
-        print(ids_on_ufsc)
-
+ 
         
 
 os.system('clear')
 lattes = extractLattes()
 # print(lattes.recovery_id_recursive(0))
-print(lattes.recovery_by_id("K4482196Z6"))
-#print(lattes.filter_by_location())
+print(lattes.recovery_by_id("K4762759U5"))
+#print(lattes.filter('K4258346A3'))
+
+#for i in lattes.get_data():
+    #print(str(i[1])+':'+str(lattes.filter(i[0])))
+    #print(lattes.recovery_by_id(i[0]))
