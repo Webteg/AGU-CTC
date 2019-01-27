e0fdc4b82d18d6adc29fc2f7cb73277f0847eb2e
RogerAmaro <roger_amaros@hotmail.com>
2019-01-27 15:51:18 -0200

    "inserçao do banco de dados professores"

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/lab-pesquisa/Desktop/AGU-CTC/env"
+set -gx VIRTUAL_ENV "/home/roger/Área de Trabalho/AGU-CTC/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
